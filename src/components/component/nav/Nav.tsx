fb1c7dad72698ee19266f000ac282bc81ffd92d5
Petr Zabransky <info@petrzabransky.cz>
2023-02-23 14:54:59 +0100

    Add Nav Items and Styles

--- src/components/component/nav/Nav.tsx
+++ src/components/component/nav/Nav.tsx
@@ -2,9 +2,19 @@ import './Nav.scss';
 
 const Nav = () => {
 	return (
-		<div>
+		<nav className="nav">
 
-		</div>
+			<img className="nav__hamburger nav__icon" src="../..//img/ikony/hamburger.svg" alt="otevřít hlavní menu" />
+			<img className="nav__close nav__icon d-none" src="./img/ikony/zavrit.svg" alt="zavřít hlavní menu" />
+			<ul id="navigation" className="nav__menu">
+				<li className="nav__item"><a href="#" className="nav__link">úvod</a></li>
+				<li className="nav__item"><a href="#" className="nav__link">reference</a></li>
+				<li className="nav__item"><a href="#" className="nav__link">technologie</a></li>
+				<li className="nav__item"><a href="#" className="nav__link">o mě</a></li>
+				<li className="nav__item"><a href="#" className="nav__link">kontakt</a></li>
+			</ul>
+
+		</nav >
 	)
 }
 
