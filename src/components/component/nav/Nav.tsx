49f4d473ee670be5f4482cc3892549dd667cfa65
Petr Zabransky <info@petrzabransky.cz>
2023-02-23 16:30:56 +0100

    Add Images - Icons and Peter

--- src/components/component/nav/Nav.tsx
+++ src/components/component/nav/Nav.tsx
@@ -1,19 +1,21 @@
 import './Nav.scss';
+import '../../../styles/layout.scss'
 
 const Nav = () => {
 	return (
 		<nav className="nav">
+			<div className="nav__bar container">
+				<img className="nav__hamburger nav__icon" src={'./hamburger.svg'} alt="otevřít hlavní menu" />
+				<img className="nav__close nav__icon d-none" src="./img/ikony/zavrit.svg" alt="zavřít hlavní menu" />
+				<ul id="navigation" className="nav__menu">
+					<li className="nav__item"><a href="#" className="nav__link">úvod</a></li>
+					<li className="nav__item"><a href="#" className="nav__link">reference</a></li>
+					<li className="nav__item"><a href="#" className="nav__link">technologie</a></li>
+					<li className="nav__item"><a href="#" className="nav__link">o mě</a></li>
+					<li className="nav__item"><a href="#" className="nav__link">kontakt</a></li>
+				</ul>
 
-			<img className="nav__hamburger nav__icon" src="../..//img/ikony/hamburger.svg" alt="otevřít hlavní menu" />
-			<img className="nav__close nav__icon d-none" src="./img/ikony/zavrit.svg" alt="zavřít hlavní menu" />
-			<ul id="navigation" className="nav__menu">
-				<li className="nav__item"><a href="#" className="nav__link">úvod</a></li>
-				<li className="nav__item"><a href="#" className="nav__link">reference</a></li>
-				<li className="nav__item"><a href="#" className="nav__link">technologie</a></li>
-				<li className="nav__item"><a href="#" className="nav__link">o mě</a></li>
-				<li className="nav__item"><a href="#" className="nav__link">kontakt</a></li>
-			</ul>
-
+			</div>
 		</nav >
 	)
 }
