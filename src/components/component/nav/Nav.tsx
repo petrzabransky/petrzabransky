9296b2e89059b624075f6c17824ad991b8195041
Petr Zabransky <info@petrzabransky.cz>
2023-03-01 17:47:52 +0100

    Nav Clasic <A Href> Links

--- src/components/component/nav/Nav.tsx
+++ src/components/component/nav/Nav.tsx
@@ -1,11 +1,7 @@
 import "./Nav.scss";
 import "../../../styles/container.scss"
-import { Link } from "react-scroll";
 
 const Nav = () => {
-	const delay: number = 0;
-	const duration: number = 0;
-	const offset: number = 0;
 
 	return (
 		<nav className="nav">
@@ -13,11 +9,11 @@ const Nav = () => {
 				<img className="nav__hamburger nav__icon" src={"./hamburger.svg"} alt="otevřít hlavní menu" />
 				<img className="nav__close nav__icon d-none" src="./img/ikony/zavrit.svg" alt="zavřít hlavní menu" />
 				<ul id="navigation" className="nav__menu">
-					<li><Link to="hero" spy={true} smooth={true} delay={delay} duration={duration} offset={offset} >úvod</Link></li>
-					<li><Link to="reference" spy={true} smooth={true} delay={delay} duration={duration} offset={offset} >reference</Link></li>
-					<li><Link to="technology" spy={true} smooth={true} delay={delay} duration={duration} offset={offset} >technologie</Link></li>
-					<li><Link to="about" spy={true} smooth={true} delay={delay} duration={duration} offset={offset} >o mě</Link></li>
-					<li><Link to="contact" spy={true} smooth={true} delay={delay} duration={duration} offset={offset} >kontakt</Link></li>
+					<li className="nav__item"><a className="nav__link" href="#hero">úvod</a></li>
+					<li className="nav__item"><a className="nav__link" href="#reference">reference</a></li>
+					<li className="nav__item"><a className="nav__link" href="#technology">technologie</a></li>
+					<li className="nav__item"><a className="nav__link" href="#about">o mě</a></li>
+					<li className="nav__item"><a className="nav__link" href="#contact">kontakt</a></li>
 				</ul>
 			</div>
 		</nav >
