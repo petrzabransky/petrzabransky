ad7b7db5925ffd4b38e4f52920cea8c1b6cc9604
Petr Zabransky <info@petrzabransky.cz>
2023-03-01 19:38:29 +0100

    Add Technology section

--- src/components/block/technology/Technology.tsx
+++ src/components/block/technology/Technology.tsx
@@ -9,18 +9,16 @@ const Technology = () => {
 				<h2 className="heading heading--header">Technologie</h2>
 				<div className="grid grid--4">
 					<span className="hashtag hashtag--big">html</span>
-					<span className="hashtag hashtag--big">typescript</span>
-					<span className="hashtag hashtag--big">git</span>
-					<span className="hashtag hashtag--big">gulp</span>
-
 					<span className="hashtag hashtag--big">css</span>
-					<span className="hashtag hashtag--big">oop a moduly</span>
-					<span className="hashtag hashtag--big">sass</span>
-					<span className="hashtag hashtag--big">webpack</span>
-
 					<span className="hashtag hashtag--big">javascript</span>
+					<span className="hashtag hashtag--big">typescript</span>
+					<span className="hashtag hashtag--big">oop a moduly</span>
 					<span className="hashtag hashtag--big">ajax</span>
+					<span className="hashtag hashtag--big">git</span>
+					<span className="hashtag hashtag--big">sass</span>
 					<span className="hashtag hashtag--big">bem</span>
+					<span className="hashtag hashtag--big">gulp</span>
+					<span className="hashtag hashtag--big">webpack</span>
 				</div>
 			</div>
 		</section>
