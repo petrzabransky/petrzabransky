609c09cb9bc8bafd2d0e5d2ce185e3ff1a1d94a2
Petr Zabransky <info@petrzabransky.cz>
2023-03-01 17:39:46 +0100

    Load Montserrat Font

--- src/components/block/Hero.tsx
+++ src/components/block/Hero.tsx
@@ -1,11 +1,13 @@
 import "../../styles/container.scss";
+import "Hero.scss";
 
 const Hero = () => {
 	return (
-		<section id="#hero">
+		<section className="hero" id="#hero" >
 			<div className="container">
-				<h1>HERO</h1>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ipsa architecto ut dolorem ullam eum eos quia alias veniam perferendis error, voluptatibus qui corporis placeat quod dignissimos! Ex, ipsam cupiditate!</p>
+				<h1 className="hero__title">#jsem programátor</h1>
+				<h2 className="hero__title">a hledám práci</h2>
+				<p className="hero__description">Programování bylo od malička mou největší vášní, které jsem se věnoval ve volném čase. Nyní bych se chtěl posunout a začít se programování věnovat naplno profesionálně.</p>
 			</div>
 		</section>
 	)
