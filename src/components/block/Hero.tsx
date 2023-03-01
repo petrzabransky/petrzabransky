510c4c47f8dc766f79f2857451ca167274b7456f
Petr Zabransky <info@petrzabransky.cz>
2023-03-01 18:29:49 +0100

    Style Hero section

--- src/components/block/Hero.tsx
+++ src/components/block/Hero.tsx
@@ -1,15 +1,16 @@
 import "../../styles/container.scss";
-import "Hero.scss";
+import "./Hero.scss";
 
 const Hero = () => {
 	return (
 		<section className="hero" id="#hero" >
 			<div className="container">
-				<h1 className="hero__title">#jsem programátor</h1>
-				<h2 className="hero__title">a hledám práci</h2>
-				<p className="hero__description">Programování bylo od malička mou největší vášní, které jsem se věnoval ve volném čase. Nyní bych se chtěl posunout a začít se programování věnovat naplno profesionálně.</p>
+				<h1 className="hero__title">#Jsem programátor</h1>
+				<h2 className="hero__subtitle">a hledám práci</h2>
+				<p className="hero__description">Programování bylo od malička mou největší vášní, které jsem se věnoval ve volném čase.</p>
+				<p className="hero__description">Nyní bych se chtěl posunout a začít se programování věnovat naplno profesionálně.</p>
 			</div>
-		</section>
+		</section >
 	)
 }
 
