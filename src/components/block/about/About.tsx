bf1925e67414654995add1ad4b8ed0f13b357a02
Petr Zabransky <info@petrzabransky.cz>
2023-03-03 13:47:14 +0100

    Add Reference Images

--- src/components/block/about/About.tsx
+++ src/components/block/about/About.tsx
@@ -6,102 +6,135 @@ import petr from "../../../assets/images/petr-zabransky.png";
 const About = () => {
 	return (
 		<>
-			<section className="about section" id="#about">
-				<div className="container">
-					<h2 className="heading heading--header">O mě</h2>
-					<div className="grid grid--small-big">
-						<div>
-							<img src={petr} alt="Petr Zábranský portrét" />
-						</div>
-						<div>
-							<p>
-								Na začátku své kariéry jsem pracoval v oblasti počítačové grafiky, reklamy a marketingu. Nutkání hledat ve všem systém a logiku mě pak přivedlo k techničtějšímu oboru do strojírenství, kde jsem se  věnoval práci vývovojového konstruktéra a automatizaci výrobních procesů.
-							</p>
-
-							<p>
-								Další  větší krok k programování jsem udělal, když mě v třetí fázi kariéry zaujalo algoritmické obchodování na měnové burze, kterému jsem se dalších několik let úspěšně věnoval na plný úvazek jako Full TIme Trader.
-							</p>
-
-							<p>
-								Nyní bych chtěl  spojit příjemné s užitečným a začít se programování věnovat naplno profesionálně.  Když se člověk živí svým koníčkem, dává do práce daleko více energie.  Rychle roste profesionalita i produktivita a spokojenost je pak na straně zaměstnance, zaměstnavatele i zákazníka.
-							</p>
-						</div>
-					</div>
-				</div>
-			</section >
-
 			<section className="section">
-				<div className="container">
+				<section className="about subsection" id="#about">
+					<div className="container">
+						<h2 className="heading heading--header">O mě</h2>
+						<div className="grid grid--small-big">
+							<div>
+								<img src={petr} alt="Petr Zábranský portrét" />
+							</div>
+							<div>
+								<p>
+									Na začátku své kariéry jsem pracoval v oblasti počítačové grafiky, reklamy a marketingu. Nutkání hledat ve všem systém a logiku mě pak přivedlo k techničtějšímu oboru do strojírenství, kde jsem se  věnoval práci vývovojového konstruktéra a automatizaci výrobních procesů.
+								</p>
 
-					<h3 className="heading heading--header">
-						Potenciál
-					</h3>
-					<div className="grid grid--small-big">
-						{/*<img src={ } alt="petr zabransky portrét" />*/}
-						<p>
-							Silné stránky
-						</p>
+								<p>
+									Další  větší krok k programování jsem udělal, když mě v třetí fázi kariéry zaujalo algoritmické obchodování na měnové burze, kterému jsem se dalších několik let úspěšně věnoval na plný úvazek jako Full TIme Trader.
+								</p>
 
-						<div className="wrap">
-							<p className="hashtag">analytické&nbsp;myšlení</p>
-							<p className="hashtag">řešení&nbsp;problémů</p>
-							<p className="hashtag">hledání&nbsp;systémů</p>
-							<p className="hashtag">kreativita</p>
-							<p className="hashtag">flexibilita</p>
-						</div>
-
-						<p>
-							Slabé stránky
-						</p>
-
-						<div>
-							<span className="hashtag">angličtina</span>
-						</div>
-
-						<p>
-							IQ
-						</p>
-
-						<div>
-							<span className="hashtag">129</span>
-						</div>
-
-						<p>
-							Volný čas
-						</p>
-
-						<div>
-							<span className="hashtag">programování</span>
-							<span className="hashtag">příroda</span>
-							<span className="hashtag">sport</span>
+								<p>
+									Nyní bych chtěl  spojit příjemné s užitečným a začít se programování věnovat naplno profesionálně.  Když se člověk živí svým koníčkem, dává do práce daleko více energie.  Rychle roste profesionalita i produktivita a spokojenost je pak na straně zaměstnance, zaměstnavatele i zákazníka.
+								</p>
+							</div>
 						</div>
 					</div>
-				</div>
-			</section>
+				</section >
 
-			<section className="section">
-				<div className="container">
-					<h3 className="heading heading--header">
-						Vzdělání
-					</h3>
-					<div className="grid grid--small-big">
-						<p>Strojírenství</p>
-						<div className="wrap">
-							<p className="hashtag">programování CNC strojů a robotů</p>
-							<p className="hashtag">maturita</p>
-						</div>
 
-						<p>Marketing</p>
-						<div className="wrap">
-							<p className="hashtag">CIMA-A</p>
-						</div>
+				<section className="subsection">
+					<div className="container">
 
-						<p>Informatika</p>
-						<div className="wrap">
-							<p className="hashtag">samostudium</p>
+						<h3 className="heading heading--header">
+							Potenciál
+						</h3>
+						<div className="grid grid--small-big">
+							{/*<img src={ } alt="petr zabransky portrét" />*/}
+							<h4 className="heading heading--title">Silné stránky</h4>
+
+							<div className="hashtag__container">
+								<div>
+									<span className="hashtag">analytické&nbsp;myšlení</span>
+								</div>
+
+								<div>
+									<span className="hashtag">řešení&nbsp;problémů</span>
+								</div>
+
+								<div>
+									<span className="hashtag">hledání&nbsp;systémů</span>
+								</div>
+
+								<div>
+									<span className="hashtag">kreativita</span>
+								</div>
+
+								<div>
+									<span className="hashtag">flexibilita</span>
+								</div>
+							</div>
+
+							<h4 className="heading heading--title">Slabé stránky</h4>
+
+							<div className="hashtag__container">
+								<div>
+									<span className="hashtag">angličtina</span>
+								</div>
+							</div>
+
+							<h4 className="heading heading--title">IQ</h4>
+
+							<div className="hashtag__container">
+								<div>
+									<span className="hashtag">129</span>
+								</div>
+							</div>
+
+							<h4 className="heading heading--title">
+								Volný čas
+							</h4>
+
+							<div className="hashtag__container">
+								<div>
+									<span className="hashtag">programování</span>
+								</div>
+
+								<div>
+									<span className="hashtag">příroda</span>
+								</div>
+
+								<div>
+									<span className="hashtag">sport</span>
+								</div>
+
+							</div>
 						</div>
 					</div>
-				</div>
+				</section>
+
+				<section className="subsection">
+					<div className="container">
+						<h3 className="heading heading--header">
+							Vzdělání
+						</h3>
+						<div className="grid grid--small-big">
+							<h4 className="heading heading--title">Strojírenství</h4>
+							<div className="hashtag__container">
+								<div>
+									<p className="hashtag">programování CNC strojů a robotů</p>
+								</div>
+
+								<div>
+									<p className="hashtag">maturita</p>
+								</div>
+							</div>
+
+							<h4 className="heading heading--title">Marketing</h4>
+							<div className="hashtag__container">
+								<div>
+									<p className="hashtag">CIMA-A</p>
+								</div>
+							</div>
+
+							<h4 className="heading heading--title">Informatika</h4>
+							<div className="hashtag__container">
+								<div>
+									<p className="hashtag">samostudium</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
 			</section>
 		</>
 	)
--- src/components/block/reference/Reference.tsx
+++ src/components/block/reference/Reference.tsx
@@ -4,16 +4,7 @@ const Reference = () => {
 	return (
 		<section id="#reference">
 			<div className="container">
-				<h1>REFERENCE</h1>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ipsa architecto ut dolorem ullam eum eos quia alias veniam perferendis error, voluptatibus qui corporis placeat quod dignissimos! Ex, ipsam cupiditate!</p>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ipsa architecto ut dolorem ullam eum eos quia alias veniam perferendis error, voluptatibus qui corporis placeat quod dignissimos! Ex, ipsam cupiditate!</p>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ipsa architecto ut dolorem ullam eum eos quia alias veniam perferendis error, voluptatibus qui corporis placeat quod dignissimos! Ex, ipsam cupiditate!</p>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ipsa architecto ut dolorem ullam eum eos quia alias veniam perferendis error, voluptatibus qui corporis placeat quod dignissimos! Ex, ipsam cupiditate!</p>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ipsa architecto ut dolorem ullam eum eos quia alias veniam perferendis error, voluptatibus qui corporis placeat quod dignissimos! Ex, ipsam cupiditate!</p>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ipsa architecto ut dolorem ullam eum eos quia alias veniam perferendis error, voluptatibus qui corporis placeat quod dignissimos! Ex, ipsam cupiditate!</p>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ipsa architecto ut dolorem ullam eum eos quia alias veniam perferendis error, voluptatibus qui corporis placeat quod dignissimos! Ex, ipsam cupiditate!</p>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ipsa architecto ut dolorem ullam eum eos quia alias veniam perferendis error, voluptatibus qui corporis placeat quod dignissimos! Ex, ipsam cupiditate!</p>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum ipsa architecto ut dolorem ullam eum eos quia alias veniam perferendis error, voluptatibus qui corporis placeat quod dignissimos! Ex, ipsam cupiditate!</p>
+
 			</div>
 		</section>
 	)
