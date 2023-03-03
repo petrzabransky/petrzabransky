d9f863f883cf2e847e26ff87a2625af9b5c13c90
Petr Zabransky <info@petrzabransky.cz>
2023-03-03 16:38:35 +0100

    Add Footer content

--- src/components/partial/footer/Footer.tsx
+++ src/components/partial/footer/Footer.tsx
@@ -1,10 +1,31 @@
 import "./Footer.scss";
+import Nav from "../../component/nav/Nav"
 
 const Footer = () => {
 	return (
-		<footer>
+		<footer className="footer">
 			<div className="container">
-				<p>Any footer</p>
+				<div className="grid grid--3">
+					<div>
+						<a href="#hero" className="footer__link">Petr Zábranský</a>
+					</div>
+
+					<ul className="footer__menu">
+						<li className="footer__item"><a className="footer__link" href="#hero">úvod</a></li>
+						<li className="footer__item"><a className="footer__link" href="#reference">reference</a></li>
+						<li className="footer__item"><a className="footer__link" href="#technology">technologie</a></li>
+						<li className="footer__item"><a className="footer__link" href="#about">o mě</a></li>
+						<li className="footer__item"><a className="footer__link" href="#contact">kontakt</a></li>
+					</ul>
+
+					<ul className="footer__menu">
+						<li className="footer__contact">+420 736 643 377</li>
+						<li className="footer__contact">info@petrzabransky.cz</li>
+						<li className="footer__contact">www.github.com/petrzabransky</li>
+					</ul>
+
+
+				</div>
 			</div>
 		</footer>
 	)
