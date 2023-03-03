b649480f20692eadd08a15d4fc15b978c8160b27
Petr Zabransky <info@petrzabransky.cz>
2023-03-03 13:59:24 +0100

    Update Hero text

--- src/components/block/hero/Hero.tsx
+++ src/components/block/hero/Hero.tsx
@@ -7,8 +7,8 @@ const Hero = () => {
 			<div className="container">
 				<h1 className="hero__title">#Jsem programátor</h1>
 				<h2 className="hero__subtitle">a hledám práci</h2>
-				<p className="hero__description">Programování bylo od malička mou největší vášní, které jsem se věnoval ve volném čase.</p>
-				<p className="hero__description">Nyní bych se chtěl posunout a začít se programování věnovat naplno profesionálně.</p>
+				<p className="hero__description">Programování bylo od malička mým největším komníčkem,</p>
+				<p className="hero__description">kterému bych se teď chtěl začít věnovat naplno profesionálně</p>
 			</div>
 		</section >
 	)
