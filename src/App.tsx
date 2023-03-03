8e4a9335df642f1c5f0c871aae0dc85fe71fd592
Petr Zabransky <info@petrzabransky.cz>
2023-03-03 18:47:52 +0100

    Add Quote section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./styles/container.scss";
 import "./App.scss";
 import Hero from "./components/block/hero/Hero";
 import Reference from "./components/block/reference/Reference";
+import Quote from "./components/block/quote/Quote";
 import About from "./components/block/about/About";
 import Contact from "./components/block/contact/Contact";
 import Technology from "./components/block/technology/Technology";
@@ -15,6 +16,7 @@ function App() {
       <main>
         <section id="hero"><Hero /></section>
         <section id="reference"><Reference /></section>
+        <Quote />
         <section id="technology"><Technology /></section>
         <section id="about"><About /></section>
         <section id="contact"><Contact /></section>
---
+++ src/components/block/quote/Quote.tsx
@@ -0,0 +1,13 @@
+import "./Quote.scss";
+
+const Quote = () => {
+	return (
+		<section className="quote section">
+			<div className="container">
+				<h2 className="quote__text">Nejlepší cestou ke štěstí a úspěchu je práce, která je zároveň zábavou</h2>
+			</div>
+		</section>
+	)
+}
+
+export default Quote;
