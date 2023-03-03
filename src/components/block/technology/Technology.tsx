ac290141a0179355ef0259aa8fa9031a5aaa5240
Petr Zabransky <info@petrzabransky.cz>
2023-03-03 14:05:43 +0100

    Add React and text to Technology section

--- src/components/block/technology/Technology.tsx
+++ src/components/block/technology/Technology.tsx
@@ -8,10 +8,11 @@ const Technology = () => {
 		<section className="technology section" id="#technology">
 			<div className="container">
 				<h2 className="heading heading--header text-center">Technologie</h2>
-				<div className="grid grid--4">
+				<div className="grid grid--4 mb-5">
 					<span className="hashtag hashtag--big">html</span>
 					<span className="hashtag hashtag--big">css</span>
 					<span className="hashtag hashtag--big">javascript</span>
+					<span className="hashtag hashtag--big">react</span>
 					<span className="hashtag hashtag--big">typescript</span>
 					<span className="hashtag hashtag--big">oop a moduly</span>
 					<span className="hashtag hashtag--big">ajax</span>
@@ -21,6 +22,11 @@ const Technology = () => {
 					<span className="hashtag hashtag--big">gulp</span>
 					<span className="hashtag hashtag--big">webpack</span>
 				</div>
+
+				<p>
+					Úvádím technologie, které běžně používám, chápu a zvládám. Pro posouzení konkrétního rozsahu znalostí a způsobu použití můžete využít můj
+					<a href="https://www.github.com/petrzabransky" target="_blank"> github.</a> Jakoukoli jinou technologii, kterou vyžívá Vaše firma, se rád a rychle doučím.
+				</p>
 			</div>
 		</section>
 	)
