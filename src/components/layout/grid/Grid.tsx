6a2b9f2b824d62e0ab1ece787b02100d49ec50ea
Petr Zabransky <info@petrzabransky.cz>
2023-03-04 20:08:33 +0100

    Grid Gap as Component Parameter

--- src/components/layout/grid/Grid.tsx
+++ src/components/layout/grid/Grid.tsx
@@ -3,22 +3,24 @@ import "./Grid.scss";
 interface Props {
 	children: JSX.Element | JSX.Element[],
 	column?: string,
-	gapRowZero?: boolean
+	gridRowGap?: number,
+	marginBottom?: number
 }
 
-const Grid = ({ children, column, gapRowZero }: Props) => {
+const Grid = ({ children, column, gridRowGap, marginBottom }: Props) => {
 
 	let style: string;
+	let otherStyle;
 
 	if (column) {
 		style = "grid grid--" + column;
 	}
 	else style = "grid grid--3"; // Select 3 columns grid if number of columns is not specified
 
-	if (gapRowZero) style += " gap-row-0"
+	otherStyle = { gridRowGap: gridRowGap + "rem", marginBottom: marginBottom + "rem" };
 
 	return (
-		<div className={style} >
+		<div className={style} style={otherStyle}>
 			{children}
 		</div >
 	)
