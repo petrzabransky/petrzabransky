5e8cc004561608a35f6555f1fccc5f1fb7f1ced2
Petr Zabransky <info@petrzabransky.cz>
2023-03-04 17:33:21 +0100

    Grid Type 1vs3 in Contact section

--- src/components/block/contact/Contact.tsx
+++ src/components/block/contact/Contact.tsx
@@ -8,14 +8,14 @@ const Contact = () => {
 			<section>
 				<Container>
 					<h2 className="heading heading--header">Contact</h2>
-					<Grid>
+					<Grid type="1vs3">
 						<p>Mobil</p>
 						<p>+420 736 643 377</p>
 						<p>Email</p>
 						<p>info@petrzabransky.cz</p>
 					</Grid>
 
-					<Grid>
+					<Grid type="1vs3">
 						<p>Email</p>
 						<p>info@petrzabransky.cz</p>
 						<p>Web</p>
@@ -24,12 +24,12 @@ const Contact = () => {
 						<p>www.github.com/petrzabransky</p>
 					</Grid>
 
-					<Grid>
+					<Grid type="1vs3">
 						<p>Bydliště</p>
 						<p>Palkovice, okr. Frýdek-Místek, Moravskoslezský kraj, CZ</p>
 					</Grid>
 
-					<Grid>
+					<Grid type="1vs3">
 						<p>Spolupráce</p>
 						<p>IČ, HPP, DPP, Remote, HO i denně ve firmě</p>
 					</Grid>
