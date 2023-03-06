38aaf4e72dbdeadb71fc9e0b9ffcda6104fe366a
Petr Zabransky <info@petrzabransky.cz>
2023-03-06 09:31:31 +0100

    Responsive Grid with NPM react-responsive

--- src/components/layout/grid/Grid.tsx
+++ src/components/layout/grid/Grid.tsx
@@ -1,31 +1,34 @@
 import "./Grid.scss";
+import { useMediaQuery } from "react-responsive";
+
 
 interface Props {
 	children: JSX.Element | JSX.Element[],
-	col?: number,	// Number of columns
-	min?: number	// Min width of 1 column (PX)
+	col?: number[],	// Number of columns
 	gap?: number,	// Grid Gap (PX)
 	margin?: number,	// Bottom Magin under of Grid (REM)
-
-	// Old parameters
-	column?: string,
-	gridRowGap?: number,
-	marginBottom?: number
 }
 
-const Grid = ({ children, col = 1, gap = 32, margin = 3 }: Props) => {
-
+const Grid = ({ children, col = [1, 1, 1], gap = 32, margin = 3 }: Props) => {
 	let style;
-	let cont = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--container'));
-	// Count min width of column when we know only number of columns
-	let colMinWidth = (cont - ((col - 1) * gap)) / col;
 
 	// Style for grid
 	style = {
-		gridTemplateColumns: "repeat(auto-fit, minmax(" + colMinWidth + "px, 1fr)",
 		gridGap: gap,
-		marginBottom: margin + "rem"
-	}
+		marginBottom: margin + "rem",
+	};
+
+	// Breakpoints
+	const breakTablet = parseInt(getComputedStyle(document.documentElement).getPropertyValue("--breakpoint-tablet"));
+	const breakDesktop = parseInt(getComputedStyle(document.documentElement).getPropertyValue("--breakpoint-desktop"));
+
+	const isTablet = useMediaQuery({ minWidth: breakTablet });
+	const isDesktop = useMediaQuery({ minWidth: 1224 });
+
+	// Set number of columns for grid
+	if (isDesktop) Object.assign(style, { gridTemplateColumns: "repeat(" + col[2] + ",1fr)" });
+	else if (isTablet) Object.assign(style, { gridTemplateColumns: "repeat(" + col[1] + ",1fr)" });
+	else Object.assign(style, { gridTemplateColumns: "repeat(" + col[0] + ",1fr)" });
 
 	return (
 		<div className={"grid"} style={style}>
