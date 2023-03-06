682a837f2f99571d735429aab0073f8419c6eb3b
Petr Zabransky <info@petrzabransky.cz>
2023-03-06 17:18:41 +0100

    Remove class nav__link--active from Nav

--- src/components/component/nav/Nav.tsx
+++ src/components/component/nav/Nav.tsx
@@ -20,7 +20,7 @@ const Nav = () => {
 			<div className="nav__bar">
 				<img onClick={toggleHandler} className="nav__hamburger nav__icon" src={isActive ? close : hamburger} alt="otevřít hlavní menu" />
 				<ul className="nav__menu" style={{ display: isDesktop || isActive ? "flex" : "none" }}>
-					<li className="nav__item"><a className="nav__link nav__link--active" href="#hero">úvod</a></li>
+					<li className="nav__item"><a className="nav__link" href="#hero">úvod</a></li>
 					<li className="nav__item"><a className="nav__link" href="#reference">reference</a></li>
 					<li className="nav__item"><a className="nav__link" href="#technology">technologie</a></li>
 					<li className="nav__item"><a className="nav__link" href="#about">o mě</a></li>
