8a86f955be1aba53950dd85eb5ca730224d4c167
Petr Zabransky <info@petrzabransky.cz>
2023-03-07 09:44:32 +0100

    Active Nav Link Bold

--- src/components/component/nav/Nav.tsx
+++ src/components/component/nav/Nav.tsx
@@ -19,7 +19,7 @@ const Nav = () => {
 			<div className="nav__bar">
 				<img onClick={toggleHandler} className="nav__hamburger nav__icon" src={isActive ? close : hamburger} alt="otevřít hlavní menu" />
 				<ul className={isActive ? "nav__menu" : "nav__menu nav__menu--hide"}>
-					<li className="nav__item"><a className="nav__link" href="#hero">úvod</a></li>
+					<li className="nav__item"><a className="nav__link nav__link--active" href="#hero">úvod</a></li>
 					<li className="nav__item"><a className="nav__link" href="#reference">reference</a></li>
 					<li className="nav__item"><a className="nav__link" href="#technology">technologie</a></li>
 					<li className="nav__item"><a className="nav__link" href="#about">o mě</a></li>
