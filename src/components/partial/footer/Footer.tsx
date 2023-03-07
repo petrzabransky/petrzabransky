da095d739b1ca5a6a875df351bdaea5a1e9f12e6
Petr Zabransky <info@petrzabransky.cz>
2023-03-07 10:39:15 +0100

    Fix Footer Grid

--- src/components/partial/footer/Footer.tsx
+++ src/components/partial/footer/Footer.tsx
@@ -6,26 +6,24 @@ const Footer = () => {
 	return (
 		<footer className="footer">
 			<Container>
-				<Grid>
-					<>
-						<div>
-							<a href="#hero" className="footer__link">Petr Zábranský</a>
-						</div>
+				<Grid col={"31"}>
+					<div>
+						<a href="#hero" className="footer__link">Petr Zábranský</a>
+					</div>
 
-						<ul className="footer__menu">
-							<li className="footer__item"><a className="footer__link" href="#hero">úvod</a></li>
-							<li className="footer__item"><a className="footer__link" href="#reference">reference</a></li>
-							<li className="footer__item"><a className="footer__link" href="#technology">technologie</a></li>
-							<li className="footer__item"><a className="footer__link" href="#about">o mě</a></li>
-							<li className="footer__item"><a className="footer__link" href="#contact">kontakt</a></li>
-						</ul>
+					<ul className="footer__menu">
+						<li className="footer__item"><a className="footer__link" href="#hero">úvod</a></li>
+						<li className="footer__item"><a className="footer__link" href="#reference">reference</a></li>
+						<li className="footer__item"><a className="footer__link" href="#technology">technologie</a></li>
+						<li className="footer__item"><a className="footer__link" href="#about">o mě</a></li>
+						<li className="footer__item"><a className="footer__link" href="#contact">kontakt</a></li>
+					</ul>
 
-						<ul className="footer__menu">
-							<li className="footer__contact">+420 736 643 377</li>
-							<li className="footer__contact">info@petrzabransky.cz</li>
-							<li className="footer__contact">www.github.com/petrzabransky</li>
-						</ul>
-					</>
+					<ul className="footer__menu">
+						<li className="footer__contact">+420 736 643 377</li>
+						<li className="footer__contact">info@petrzabransky.cz</li>
+						<li className="footer__contact">www.github.com/petrzabransky</li>
+					</ul>
 				</Grid>
 			</Container>
 		</footer >
