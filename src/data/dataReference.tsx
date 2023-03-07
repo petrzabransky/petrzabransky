8d828030f7a26fd98a871384a571f066b9e75200
Petr Zabransky <info@petrzabransky.cz>
2023-03-07 12:11:57 +0100

    Add Reference Data

---
+++ src/data/dataReference.tsx
@@ -0,0 +1,50 @@
+import fraktaly from "../assets/images/reference/fraktaly.jpg";
+import marelen from "../assets/images/reference/marelen.jpg";
+import truhlarstvi from "../assets/images/reference/truhlarstvi-js.jpg";
+import jkm from "../assets/images/reference/jkm-family.jpg";
+
+const dataReference = [
+	{
+		id: 1,
+		image: fraktaly,
+		title: "Faktály",
+		technology: "TYPESCRIPT | OOP",
+		description: "Web o fraktálech s interaktivním kreslením",
+		website: "www.fraktly.cz",
+		url: "https://www.fraktaly.cz",
+		github: "www.github.com/petrzabransky/fraktaly"
+	},
+	{
+		id: 2,
+		image: marelen,
+		title: "Marelen",
+		technology: "ASP.NET | C#",
+		description: "Dynamický web pro salon krásy",
+		website: "www.marelen.cz",
+		url: "https://www.marelen.cz",
+		github: "www.github.com/petrzabransky/marelen"
+	},
+	{
+		id: 3,
+		image: truhlarstvi,
+		title: "Truhlářství JS",
+		technology: "HTML | CSS | JAVASCRIPT",
+		description: "Web pro rodinné truhlářství s galerii",
+		website: "www.truhlarstvijs.cz",
+		url: "https://www.truhlarstvijs.cz",
+		github: ""
+	},
+	{
+		id: 4,
+		image: jkm,
+		title: "JKM Family",
+		technology: "HTML | CSS",
+		description: "Kódování webu pro relitní kancelář",
+		website: "www.jkmfamily.cz",
+		url: "https://www.jkmfamily.cz",
+		github: ""
+	},
+
+]
+
+export default dataReference;
